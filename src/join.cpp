f06abe8eadec1f86d5cbc0bfb919e757ee863cf5
Filipi <filipi565@hotmail.com>
2024-10-27 18:56:20 -0300

    Bug fix

--- src/join.cpp
+++ src/join.cpp
@@ -10,52 +10,44 @@ using namespace std;
 
 OS_PUBLIC int os::path::join(char *buff, size_t s, const initializer_list<const char *> &parts)
 {
-    string temp;
-    const char **paths = (const char **)malloc((parts.size()+1) * sizeof(void *));
+    try
+    {
+        string temp;
 
-    if (paths == nullptr)
+        auto first = (*parts.begin());
+
+        bool need_cwd = (
+            first[0] == '.' && (IS_SEP(first[1]) ||
+            first[1] == '.' && IS_SEP(first[2]))
+        );
+
+        if (need_cwd)
+        {
+            char cwd[FILENAME_MAX];
+            os::getcwd(cwd, FILENAME_MAX);
+
+            temp = cwd;
+            temp += os::SEP;
+        }
+
+        for (auto part : parts)
+        {
+            temp += part;
+            temp += os::SEP;
+        }
+
+        temp.pop_back(); // Remove trailing separator
+
+        char new_buff[FILENAME_MAX];
+
+        std::char_traits<char>::copy(new_buff, temp.c_str(), min(s, temp.size()));
+
+        cwk_path_normalize(new_buff, buff, s);
+    }
+    catch (const exception &e)
     {
         return 1;
     }
-
-    if (!parts.size())
-    {
-        (*buff) = 0;
-        return 0;
-    }
-
-    auto _first = (*parts.begin());
-
-    char start = 0;
-
-    bool _need_cwd = (
-        ( _first[0] == '.' && IS_SEP(_first[1]) ) ||
-        ( _first[0] == '.' && _first[1] == '.' && IS_SEP(_first[2]) )
-    );
-
-    if (_need_cwd)
-    {
-        char buffer[FILENAME_MAX];
-        os::getcwd(buffer, FILENAME_MAX);
-
-        temp = buffer;
-        temp += os::SEP;
-        temp += _first;
-
-        start = 1;
-        paths[0] = temp.c_str();
-    }
-
-    for (size_t i = start; i < parts.size(); i++)
-    {
-        paths[i] = (*parts.begin()) + i;
-    }
-
-    paths[parts.size()] = nullptr;
-
-    cwk_path_join_multiple(paths, buff, s);
-
-    free(paths);
 
     return 0;
 }
