60cf13b1dee91853f245fca2c37051ac01fc80a6
Filipi <filipi565@hotmail.com>
2024-10-27 11:29:11 -0300

    Bug fix

--- src/listdir.cpp
+++ src/listdir.cpp
@@ -8,45 +8,51 @@
 #include <dirent.h>
 #endif
 
+#include <vector>
+#include <string>
+
 using namespace std;
 
-OS_PUBLIC vector<string> os::listdir(const char *path)
+namespace os
 {
-    if (!os::path::exists(path))
+    OS_PUBLIC vector<string> listdir(const char *path)
     {
-        throw invalid_argument("Path does not exist: " + string(path));
-    }
-
-    if (!os::path::is_dir(path))
-    {
-        throw invalid_argument("Path is not a directory: " + string(path));
-    }
-
-    vector<string> result;
-
-    dirent **namelist;
-
-    int n = scandir(path, &namelist, nullptr, alphasort);
-    if (n < 0)
-    {
-        throw runtime_error("Error scanning directory: " + string(path));
-    }
-    else
-    {
-        for (int i = 0; i < n; i++)
+        if (!os::path::exists(path))
         {
-            if (namelist[i]->d_name == string(".") || namelist[i]->d_name == string(".."))
-            {
-                free(namelist[i]);
-                continue;
-            }
-
-            result.push_back(namelist[i]->d_name);
-            free(namelist[i]);
+            throw invalid_argument("Path does not exist: " + string(path));
         }
-        free(namelist);
-    }
 
-    result.shrink_to_fit();
-    return result;
+        if (!os::path::is_dir(path))
+        {
+            throw invalid_argument("Path is not a directory: " + string(path));
+        }
+
+        vector<string> result;
+
+        dirent **namelist;
+
+        int n = scandir(path, &namelist, nullptr, alphasort);
+        if (n < 0)
+        {
+            throw runtime_error("Error scanning directory: " + string(path));
+        }
+        else
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (namelist[i]->d_name == string(".") || namelist[i]->d_name == string(".."))
+                {
+                    free(namelist[i]);
+                    continue;
+                }
+
+                result.push_back(namelist[i]->d_name);
+                free(namelist[i]);
+            }
+            free(namelist);
+        }
+
+        result.shrink_to_fit();
+        return result;
+    }
 }
