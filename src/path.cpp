1b2e9767ad297fa685063cd0dc8ab433d96c78cc
Filipi <filipi565@hotmail.com>
2024-10-29 13:38:23 -0300

    Bug fix for msvc

--- src/path.cpp
+++ src/path.cpp
@@ -6,36 +6,39 @@
 #define STAT NAME(stat)
 #define LSTAT NAME(lstat)
 
-namespace os::path
+namespace os
 {
-    OS_PUBLIC bool exists(const char *path)
+    namespace path
     {
-        struct STAT info;
-
-        return (STAT(path, &info) == 0);
-    }
-
-    OS_PUBLIC bool is_dir(const char *path)
-    {
-        struct STAT info;
-        
-        if (STAT(path, &info) != 0)
+        OS_PUBLIC bool exists(const char *path)
         {
-            return false;
+            struct STAT info;
+
+            return (STAT(path, &info) == 0);
         }
 
-        return S_ISDIR(info.st_mode);
-    }
-
-    OS_PUBLIC bool is_file(const char *path)
-    {
-        struct STAT info;
-        
-        if (STAT(path, &info) != 0)
+        OS_PUBLIC bool is_dir(const char *path)
         {
-            return false;
+            struct STAT info;
+            
+            if (STAT(path, &info) != 0)
+            {
+                return false;
+            }
+
+            return S_ISDIR(info.st_mode);
         }
 
-        return S_ISREG(info.st_mode);
+        OS_PUBLIC bool is_file(const char *path)
+        {
+            struct STAT info;
+            
+            if (STAT(path, &info) != 0)
+            {
+                return false;
+            }
+
+            return S_ISREG(info.st_mode);
+        }
     }
 }
