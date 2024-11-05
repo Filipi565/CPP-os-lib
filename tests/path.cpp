92faf19fc42545db6c6097c35fa7e5919c9ffc9e
Filipi <filipi565@hotmail.com>
2024-11-05 19:15:22 -0300

    Add testing for os::path

---
+++ tests/path.cpp
@@ -0,0 +1,18 @@
+#include <libos/os.h>
+#include <iostream>
+
+int main(int, const char *argv[])
+{
+    char *buffer = new char[4096];
+
+    os::path::join(buffer, 4096, {argv[0], ".."});
+
+    std::cout << "Path: " << buffer << '\n';
+    std::cout << "Exists: " << os::path::exists(buffer) << '\n';
+    std::cout << "Is dir: " << os::path::is_dir(buffer) << '\n';
+    std::cout << "Is file: " << os::path::is_file(buffer) << '\n';
+
+    delete[] buffer;
+    
+    return 0;
+}
